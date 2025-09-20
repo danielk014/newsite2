1cbc6b7932c34647b7c187946e6427ba537a1fd3
Daniel Kusakci <danielkusakci@Daniels-Mini.home>
2025-09-20 11:42:52 +0200

    Revert "Implement Meta Pixel using official Next.js pattern"
    This reverts commit 4462cdedcfcab595485d595d4867da432b182409.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
+import { analyticsConfig } from "@/config/analytics";
 import { Navigation } from "@/components/layout/navigation";
-import { FB_PIXEL_ID } from "@/lib/fpixel";
-import PageView from "@/components/analytics/page-view";
+import { MetaPixel } from "@/components/analytics/meta-pixel";
 
 const inter = Inter({ 
   subsets: ["latin"],
@@ -70,39 +69,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="dark">
       <head>
-        {/* Meta Pixel Code */}
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${FB_PIXEL_ID}');
-            fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-            alt=""
-          />
-        </noscript>
+        {analyticsConfig.metaPixel.enabled && (
+          <MetaPixel pixelId={analyticsConfig.metaPixel.pixelId} />
+        )}
       </head>
       <body
         className={`${inter.variable} font-sans antialiased`}
       >
-        <PageView />
         <Navigation />
         <main className="pt-16">
           {children}
---
+++ src/config/analytics.ts
@@ -0,0 +1,11 @@
+export const analyticsConfig = {
+  metaPixel: {
+    pixelId: "1482839576301540",
+    enabled: true,
+  },
+  // Add other analytics services here as needed
+  // googleAnalytics: {
+  //   measurementId: "G-XXXXXXXXXX",
+  //   enabled: false,
+  // },
+}
